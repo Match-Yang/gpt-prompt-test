c592e3dcd0403beb935d689ff2c4bc4910ae394c
杨万青 <yangwanqing.me@qq.com>
2023-08-23 18:06:03 +0800

    Fix import failed

--- utils/prompt_helper.ts
+++ utils/prompt_helper.ts
@@ -1,7 +1,5 @@
-async function readPromptFromGithub(key: string) {
+export default async function readPromptFromGithub(key: string) {
     const response = await fetch(`https://raw.githubusercontent.com/Match-Yang/gpt-prompt-test/main/prompts/${key}.txt`);
-    const data = await response.text;
+    const data = await response.text();
     return data;
 }
-
-export default { readPromptFromGithub };
