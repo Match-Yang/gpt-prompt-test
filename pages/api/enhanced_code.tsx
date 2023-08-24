2d2e45280b34f17d29b65b5a61d7f821f55e17ec
杨万青 <yangwanqing.me@qq.com>
2023-08-24 14:36:23 +0800

    Update prompt

--- pages/api/enhanced_code.tsx
+++ pages/api/enhanced_code.tsx
@@ -93,7 +93,7 @@ export default async function handler(
     // We can also construct an LLMChain from a ChatPromptTemplate and a chat model.
     const chat = new ChatOpenAI({ temperature: 0 });
     const chatPrompt = ChatPromptTemplate.fromPromptMessages([
-      SystemMessagePromptTemplate.fromTemplate(SYSTEM_PROMPT),
+      SystemMessagePromptTemplate.fromTemplate("请帮我优化代码"),
       HumanMessagePromptTemplate.fromTemplate(USER_PROMPT),
     ]);
     const chainB = new LLMChain({
