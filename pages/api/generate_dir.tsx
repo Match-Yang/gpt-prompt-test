4a72d396717ba0d3d274b8a8f46e7565e09ca28a
杨万青 <yangwanqing.me@qq.com>
2023-08-24 15:14:19 +0800

    Update prompt

--- pages/api/generate_dir.tsx
+++ pages/api/generate_dir.tsx
@@ -8,9 +8,23 @@ import {
 import { LLMChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { SystemChatMessage } from "langchain/schema";
-import readPromptFromGithub from "utils/prompt_helper"
 
-const SYSTEM_PROMPT = ``;
+const USER_PROMPT = `
+You are a professional documentation engineer, and your task is to create a documentation website. I will provide you with a brief description of the document's topic, its type, and the intended audience.
+Please generate a directory tree suitable for this documentation website based on the given information. Generate the directory tree according to the specific details, providing as much detail as possible. The directory tree should have a minimum of two levels and a maximum of three levels, with the final level being the files. Please include at least three top-level directories. All names should be in English.
+Return the result to me in JSON format. Here is an example of the JSON format:
+
+Question:
+The document's topic is [Payment SDK Introduction], the document type is [Development Documentation], and it is targeted towards [Beginner Developers].
+
+Answer:
+{{"name": "Introduction","type": "folder","path": "Introduction","toc": [{{"name": "Getting Started","type": "file","path": "Introduction/Getting Started","toc": []}}]}}
+
+Question: 
+The document's topic is [{doc_desc}], the document type is [{doc_type}], and it is targeted towards [{target_reader_type}].
+
+Answer:
+`;
 
 export default async function handler(
     req: NextApiRequest,
@@ -28,9 +42,8 @@ export default async function handler(
         // We can also construct an LLMChain from a ChatPromptTemplate and a chat model.
         const chat = new ChatOpenAI({ temperature: 0.5 });
 
-        const userPrompt = await readPromptFromGithub("generate_dir")
         const chatPrompt = new PromptTemplate({
-            template: userPrompt,
+            template: USER_PROMPT,
             inputVariables: ["doc_desc", "doc_type", "target_reader_type"],
           });
         const chainB = new LLMChain({
@@ -38,7 +51,7 @@ export default async function handler(
             llm: chat,
         });
 
-        const systemMessage = new SystemChatMessage(SYSTEM_PROMPT);
+        const systemMessage = new SystemChatMessage("");
 
         const response = await chainB.call({
             doc_desc,
--- pages/api/generate_outline.tsx
+++ pages/api/generate_outline.tsx
@@ -8,9 +8,18 @@ import {
 import { LLMChain } from "langchain/chains";
 import { ChatOpenAI } from "langchain/chat_models/openai";
 import { SystemChatMessage } from "langchain/schema";
-import readPromptFromGithub from "utils/prompt_helper"
 
-const SYSTEM_PROMPT = ``;
+const USER_PROMPT = `
+As a professional tech writer, your task is to create an outline for an English documentation about {more_info}. The documentation should be concise and suitable for beginners to read, while also targeting {target_reader_type} as the primary audience.
+
+The output should only include the title without anything else. Please format the outline using the following structure:
+#Heading 1
+##Heading 2
+
+Your outline should provide a clear and logical structure for the documentation, organizing the content in a way that makes it easy for beginners to follow and understand. It should cover the necessary topics and provide a comprehensive {doc_type}, and how {target_reader_type} can use it effectively in their projects.
+
+Please note that your outline should be flexible enough to allow for various relevant and creative headings and subheadings.
+`;
 
 export default async function handler(
     req: NextApiRequest,
@@ -28,9 +37,8 @@ export default async function handler(
         // We can also construct an LLMChain from a ChatPromptTemplate and a chat model.
         const chat = new ChatOpenAI({ temperature: 0.5 });
 
-        const userPrompt = await readPromptFromGithub("generate_outline")
         const chatPrompt = new PromptTemplate({
-            template: userPrompt,
+            template: USER_PROMPT,
             inputVariables: ["more_info", "doc_type", "target_reader_type"],
           });
         const chainB = new LLMChain({
@@ -38,7 +46,7 @@ export default async function handler(
             llm: chat,
         });
 
-        const systemMessage = new SystemChatMessage(SYSTEM_PROMPT);
+        const systemMessage = new SystemChatMessage("");
 
         const response = await chainB.call({
             more_info,
